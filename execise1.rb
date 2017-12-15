11f00821d6abbaa624e6599135adec87276c5db4
paulcosta2001@gmail.com <paulcosta2001@gmail.com>
2017-12-15 16:09:04 -0500

    execise1 upd1

--- execise1.rb
+++ execise1.rb
@@ -1,106 +1,59 @@
+venues = [
+{ address: "123 Main Street", city: "Toronto", wheelchair_accessible: true, capacity: 100 },
+{ address: "567 Centre Street", city: "Toronto", wheelchair_accessible: false, capacity: 400 },
+{ address: "9B Ontario Street", city: "Montreal", wheelchair_accessible: true, capacity: 1000 },
+{ address: "56 Road Avenue", city: "Ottawa", wheelchair_accessible: true, capacity: 650 },
+{ address: "938 Avenue Way East", city: "Toronto", wheelchair_accessible: false, capacity: 90 },
+{ address: "34 Main Street West", city: "London", wheelchair_accessible: false, capacity: 300 },
+{ address: "44 Quebec Road", city: "Toronto", wheelchair_accessible: true, capacity: 200 },
+{ address: "10 Spruce Avenue Ouest", city: "Montreal", wheelchair_accessible: false, capacity: 525 }
 
-
-[
-{train: "72C", frequency_in_minutes: 15, direction: "north"},
-{train: "72D", frequency_in_minutes: 15, direction: "south"},
-{train: "610", frequency_in_minutes: 5, direction: "north"},
-{train: "611", frequency_in_minutes: 5, direction: "south"},
-{train: "80A", frequency_in_minutes: 30, direction: "east"},
-{train: "80B", frequency_in_minutes: 30, direction: "west"},
-{train: "110", frequency_in_minutes: 15, direction: "north"},
-{train: "111", frequency_in_minutes: 15, direction: "south"}
-]
-
-
-class train
+class wheelchair_accessible
   # Required & Special!!!
-def initialize(train)
+def initialize(address)
 # "Instance Vrainariables"
-@train = train
-@awake = true
-@tail   false
+@venues = Toronto
+@capacity = < 150
+#@capacity = 150 <= true
+#@capacity = 150 >= false
+
+venues.each do |venues|
+
+  p "#{capacity} of #{venue}"
+    venues.each do ||
+  end
 end
 
-
-class frequency_in_minutes
-# Required & Special!!!
-def initialize(frequency_in_minutes)
-# "Instance Variables"
-@frequency_in_minutes = frequency_in_minutes
-@awake      = true
-@tail       = false
-end
-
-
-class direction
-# Required & Special!!!
-def initialize(direction)
-# "Instance Variables"
-@direction = direction
-@awake      = true
-@tail       = false
-end
-
-
-
- Regular Instance Method
-# READER
-
-  def train
-    @train
-  end
-
-#READER
-  def frequency_in_minutes
-    @frequency_in_minutes
-  end
-
-# READER
-  def direction
-    @direction
-  end
-
-WRITER
-  def train=(train)
-    @train = train
-  end
-
-  # WRITER
-  def frequency_in_minutes=(frequency_in_minutes)
-    @frequency_in_minutes = frequency_in_minutes
-  end
-
-  # WRITER
-  def direction=(direction)
-    @direction = direction
-  end
-
-  # Regular Instance Method
-  def full_name
-    "#{ first_name } #{ last_name }"
-  end
-
-  def greetings
-    if @awake
-      if @tail
-        '(bounces away)'
-      else
-        "Hi, my name is #{ full_name }"
-      end
-    else
-      'Zzzzz'
-    end
-  end
-  def slumber
-    @awake = false
-  end
-
-  def wake
-    @awake = true
-  end
-
-  def mutate
-    @tail = true
-  end
+if
+puts "Wheelchair accessible venues in #{Toronto}."
 
 end
+
+# Write a program that combines suits and numbers into
+# all possible cards in a card deck
+#
+#suits = ["spades", "hearts", "diamonds", "clovers"]
+#numbers = [
+#   "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"
+# ]
+#
+# # card = ["A of spades", "2 of spades", ... ]
+#
+# suits.each do |suit|
+#   # p "A of " + suit
+#   # p "#{numbers.first} of #{suit}"
+#   # p "#{numbers[0]} of #{suit}"
+#   # p "#{numbers[1]} of #{suit}"
+#   # p "#{numbers[2]} of #{suit}"
+#
+#   # numbers.length.times do |i|
+#   #   # p numbers[i]
+#   #   p "#{numbers[i]} of #{suit}"
+#   # end
+#
+#   p "#{number} of #{suit}"
+#     numbers.each do |number|
+#   end
+# end
+#
+#
